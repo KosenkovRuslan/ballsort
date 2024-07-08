6bacb55ab8e88b98fa47fccf8223fe161e1cbf85
Ruslan Kosenkov <rkosenkov@kvant-is.ru>
2024-07-08 18:40:46 +0300

    feat: создать класс для элемента tube и описать логику

---
+++ tube/tube.ts
@@ -0,0 +1,33 @@
+namespace $ {
+    export class $games_ballsort_tube extends $mol_object {
+
+		@ $mol_mem
+        balls(next?: $games_ballsort_ball[]) {
+            return next ?? []
+        }
+
+		size() {
+			return 0
+		}
+
+		@ $mol_mem
+		complete() {
+			const [ ball, ...balls ] = this.balls()
+			return this.balls().length === this.size() && balls.every( obj => obj.color() === ball.color() )
+		}
+
+		@ $mol_action
+		take() {
+			const next = this.balls().slice()
+			const ball = next.pop()
+			this.balls( next )
+			return ball
+		}
+
+		@ $mol_action
+		put(obj: $games_ballsort_ball) {
+			this.balls( [ obj, ...this.balls() ] )
+		}
+
+    }
+}
